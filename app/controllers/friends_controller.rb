6934922bd9b671f3a9f2d8b436338e6da0e4892d
Steven King <email": null>
2016-03-01 17:03:15 -0500

    Make add friend button work

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,8 @@
 class FriendsController < ApplicationController
 
   def create
-    @friend = Friend.create(user_id: current_user.id, friend_id: )
+    @friend = Friend.create(user_id: current_user.id, friend_id: params[:friend][:friend_id])
+    redirect_to users_path
   end
 
 end
